000675ce77ed28fd4cdc67ad6518dd8b714345c6
Muhammad Muhdar <mmuhdar17@gmail.com>
2021-05-23 05:54:55 +0700

    rollback all migration and fixed tasks migration

--- database/migrations/1621721691232_create_tasks_tables.ts
+++ database/migrations/1621721691232_create_tasks_tables.ts
@@ -7,7 +7,7 @@ export default class Tasks extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id").primary();
       table.string("name").notNullable();
-      // table.text("description").nullable();
+      table.text("description").nullable();
       table.timestamp("due_at").nullable();
       table.integer("status_id").unsigned().notNullable().defaultTo(1);
       table.integer("created_by").unsigned().references("id").inTable("users");
